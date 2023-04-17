2d35ddb447813b11862b4503206c640c322494e2
Daivols6 <daivols@internet.ru>
2023-04-18 00:44:50 +0300

    Add home_2

---
+++ home_2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace home_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите первое число");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите второе число");
+            int operand2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Выберите действие: *, /, +, -"); 
+            string sign = Console.ReadLine();
+            switch (sign)
+            {
+
+                case "+": Console.WriteLine(operand1 + operand2); break;
+                case "-": Console.WriteLine(operand1 - operand2); break;
+                case "*": Console.WriteLine(operand1 * operand2); break;
+                case "/":
+                    
+                        if (operand2 == 0)
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль невозможно.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(operand1 / operand2);
+                        }
+                break;                   
+                default: Console.WriteLine("Ошибка ввода");
+                break;
+
+            }
+        }
+    }
+}
