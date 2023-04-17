2da8fa209a7d5fb9dfe009073d3e3d189f209a2c
Daivols6 <daivols@internet.ru>
2023-04-17 23:49:03 +0300

    Добавьте файлы проекта.

---
+++ Home/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace home_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + '!');
+
+        }
+    }
+}
