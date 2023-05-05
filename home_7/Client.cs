d6f331aa4842f73472e8ae5724e18b66dd0ce6a4
Daivols6 <daivols@internet.ru>
2023-05-05 20:49:30 +0300

    Add home_7

---
+++ home_7/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static home_7.Client;
+
+namespace home_7
+{
+    public class Client : Person
+
+    {   
+        public enum ActionType
+        {
+            Order_food,
+            Swear,
+            Litter,
+            Just_looking
+        }
+        public ActionType Action { get; set; }
+        public Client() : base("", 0)
+        {
+        }
+        public Client(string name, int age, ActionType action) : base(name, age)
+        {
+             Action = action;
+        }
+
+
+        string[] names = { "Roman", "Dimon", "Alex", "Vasya", "Masha", "Anna", "Vadim" };
+
+        public Client[] BuildClient(int count)
+        {
+            Client[] clients = new Client[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Random random = new Random();
+                int age = random.Next(18, 35);
+                var name = names[random.Next(0, names.Length)];
+                var action = (ActionType)random.Next(0,4);
+                clients[i] = new Client(name, age, action);
+                
+            }
+            return clients;
+        }
+        public static void PrintData(Client[] clients)
+        {
+            foreach (var client in clients)
+            {
+                Console.WriteLine($"\n Name: {client.Name}, Age: {client.Age}, Action: {client.Action}\n");
+            }
+        }
+    }
+}
---
+++ home_7/Cook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_7
+{
+    public class Cook : Person, IEmployees
+    {
+        public Cook(string name, int age) : base(name, age)
+        { }
+        
+        public void Cooking()
+        {
+            Console.WriteLine("I'am Cooking");
+        }
+        public void Cleaning()
+        {
+            Console.WriteLine("I'am Cleaning");
+        }
+
+        public override string ToString()
+        {
+            return "Order food";
+        }
+    }
+}
